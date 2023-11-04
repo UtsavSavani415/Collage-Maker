9b06175033c442ca28d12a7a47122fb4079a4f26
utsav savani <utsav@Oms-MacBook-Air.local>
2023-11-05 03:14:43 +0530

    navbar  error fixed

--- App.tsx
+++ App.tsx
@@ -1,14 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {RootNavigator} from './src/components/navbar/Navbar';
-import DashBoard from './src/components/dashboard/DashBoard';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-import {View} from 'react-native';
 
 function App(): JSX.Element {
   // const isDarkMode = useColorScheme() === 'dark';
